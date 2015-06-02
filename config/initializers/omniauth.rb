d68dedec4540fa0e07c9a9bbe8f8123bc81cf5b6
Matt Casper <matthewvcasper@gmail.com>
2015-06-02 10:44:22 -0700

    try to remove all generators

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "47720330023-fbmihitfce3vrjnol2ticonrpll14e78.apps.googleusercontent.com", "J00luebv1W9ycdZ-6YbV5W1K",
+    {
+    scope: "userinfo.email, userinfo.profile",
+    prompt: "select_account",
+    name: "google",
+    image_aspect_ratio: "square",
+    image_size: 50
+  }
+end
