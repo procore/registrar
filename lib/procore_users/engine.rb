7e5b6768c8a22530dc1cd7d9cc23a65f5e71e042
Matt Casper <matthewvcasper@gmail.com>
2015-06-02 12:05:36 -0700

    require gems through engine.rb

--- lib/procore_users/engine.rb
+++ lib/procore_users/engine.rb
@@ -1,5 +1,9 @@
 module ProcoreUsers
   class Engine < ::Rails::Engine
     isolate_namespace ProcoreUsers
+
+    require "rubygems"
+    require "omniauth"
+    require "omniauth-google-oauth2"
   end
 end
