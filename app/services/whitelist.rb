58c35b46733df6e9771258c97acc0de52061e388
Matt Casper <matthewvcasper@gmail.com>
2015-08-25 16:30:24 -0700

    Whitelist the contractors

--- app/services/whitelist.rb
+++ app/services/whitelist.rb
@@ -1,7 +1,10 @@
 class Whitelist
   def self.all
     [
-      "jeff.frost@gmail.com"
+      "jeff.frost@gmail.com",
+      "sudara@rubytune.com",
+      "peter@rubytune.com",
+      "patrik.bona@toptal.com"
     ]
   end
 end
--- spec/services/whitelist_spec.rb
+++ spec/services/whitelist_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Whitelist do
     it "returns a list of whitelisted emails" do
       expect(Whitelist.all.count).to_not eq 0
       expect(Whitelist.all).to include "jeff.frost@gmail.com"
+      expect(Whitelist.all).to include "sudara@rubytune.com"
+      expect(Whitelist.all).to include "peter@rubytune.com"
+      expect(Whitelist.all).to include "patrik.bona@toptal.com"
     end
   end
 end
